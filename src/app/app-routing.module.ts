89afa0653746d78a56966dcc15910103b8ce6ba8
Andrew Kim <akakimmykim@gmail.com>
2019-05-18 18:40:01 -0400

    hi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,22 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthorizeComponent} from './authorize/authorize.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ContactComponent} from './contact/contact.component';
+import {NewProductsComponent} from './new-products/new-products.component';
+import {TrackersComponent} from './trackers/trackers.component';
+import {TrackComponent} from './track/track.component';
+import {ReturnComponent} from './return/return.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'about', component: AboutUsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'release', component: NewProductsComponent},
+  { path: 'trackers', component: TrackersComponent},
+  { path: 'track', component: TrackComponent},
+  { path: 'return', component: ReturnComponent},
   { path: 'auth', component: AuthorizeComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AuthorizeComponent } from './authorize/authorize.component';
 import {AppRoutingModule} from './app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TrackComponent } from './track/track.component';
+import { ReturnComponent } from './return/return.component';
+import { NewProductsComponent } from './new-products/new-products.component';
+import { TrackersComponent } from './trackers/trackers.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorizeComponent,
     WelcomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    AboutUsComponent,
+    TrackComponent,
+    ReturnComponent,
+    NewProductsComponent,
+    TrackersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
